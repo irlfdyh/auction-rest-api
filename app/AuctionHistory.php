5dcc6ec64a4b425e020bc38e679c8847ecd36605
irshal <irshalfir@gmail.com>
2020-02-28 23:05:11 +0700

    somwthing is wrong

--- app/AuctionHistory.php
+++ app/AuctionHistory.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class AuctionHistory extends Model
 {
-    //
+    protected $guarded = ['history_id'];
+    protected $primaryKey = 'history_id';
+
+    public function auction() {
+        return $this->hasOne(Auction::class);
+    }
+
+    public function stuff() {
+        return $this->hasOne(Stuff::class);
+    }
+
+    public function users() {
+        return $this->belongsToMany(User::class);
+    } 
 }
--- app/User.php
+++ app/User.php
@@ -2,9 +2,11 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+//use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
-class User extends Model
+class User extends Authenticable
 {
     protected $guarded = ['user_id'];
     protected $primaryKey = 'user_id';
